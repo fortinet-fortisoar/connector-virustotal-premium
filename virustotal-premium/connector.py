182a8cf2cb90408da8787719d1155c40002f0e94
cs-shubham-morankar <smorankar@fortinet.com>
2022-10-03 14:29:38 +0530

    Added Enrichment Tags and Updated delete micro function

--- virustotal-premium/connector.py
+++ virustotal-premium/connector.py
@@ -5,7 +5,8 @@
   Copyright end """
 
 from connectors.core.connector import Connector, get_logger, ConnectorError
-from connectors.cyops_utilities.builtins import make_cyops_request
+from integrations.crudhub import make_request
+from django.conf import settings
 from .operations import operations, _check_health, MACRO_LIST
 
 logger = get_logger("virustotal-premium")
@@ -26,14 +27,16 @@ class VirusTotalPremium(Connector):
         _check_health(config)
 
     def del_micro(self, config):
-        for macro in MACRO_LIST:
-            try:
-                resp = make_cyops_request(f'/api/wf/api/dynamic-variable/?name={macro}', 'GET')
-                if resp['hydra:member']:
-                    macro_id = resp['hydra:member'][0]['id']
-                    resp = make_cyops_request(f'/api/wf/api/dynamic-variable/{macro_id}/?format=json', 'DELETE')
-            except Exception as e:
-                logger.error(e)
+        if not settings.LW_AGENT:
+            for macro in MACRO_LIST:
+                try:
+                    resp = make_request(f'/api/wf/api/dynamic-variable/?name={macro}', 'GET')
+                    if resp['hydra:member']:
+                        logger.info("resetting global variable '%s'" % macro)
+                        macro_id = resp['hydra:member'][0]['id']
+                        resp = make_request(f'/api/wf/api/dynamic-variable/{macro_id}/?format=json', 'DELETE')
+                except Exception as e:
+                    logger.error(e)
 
     def on_deactivate(self, config):
         self.del_micro(config)
